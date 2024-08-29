b9bc10d517a7d83b8f75716d2fc9adfad723c41b
DanielCT24 <166074058+DanielCT24@users.noreply.github.com>
2024-08-28 20:14:40 -0500

    Update JS - VentanaEmergente.js

--- JS - VentanaEmergente.js
+++ JS - VentanaEmergente.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const popup = document.getElementById('popup');
+    const closeBtn = document.getElementById('close-popup');
+
+    setTimeout(function() {
+        popup.classList.add('show');
+    }, 600); // Retraso de 3 segundos
+
+    closeBtn.addEventListener('click', function() {
+        popup.classList.remove('show');
+    });
+});
 
