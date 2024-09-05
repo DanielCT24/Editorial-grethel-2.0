124452f24dbc743878ade66ced9da42ea8340fdb
DanielCT24 <166074058+DanielCT24@users.noreply.github.com>
2024-09-04 20:06:18 -0500

    Add files via upload

--- JS - Header.js
+++ JS - Header.js
@@ -58,4 +58,34 @@ tabInputs.forEach(function(input) {
 
 
 
+/* ----------------------------------------TRANSICIÓN------------------------------------------------------------ */
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loader = document.getElementById('loader');
+    const links = document.querySelectorAll('.transition-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            loader.classList.remove('hidden'); // Mostrar la animación de carga
+
+            const targetUrl = this.href;
+
+            // Usar setTimeout para dar tiempo a que la animación se muestre antes de la redirección
+            setTimeout(() => {
+                window.location.href = targetUrl;
+            }, 100);
+        });
+    });
+
+    window.addEventListener('load', function() {
+        loader.classList.add('hidden'); // Ocultar la animación de carga cuando la página esté completamente cargada
+    });
+});
+
 
